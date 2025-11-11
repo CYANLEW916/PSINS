7f323122aff942358d6469593eeecd862cbb8bf7
CYANLEW916 <cyanlew64@gmail.com>
2025-11-11 16:58:30 +0800

    Update test_SINS_GPS_153.m
    commit到branch；PR到main

--- demos/test_SINS_GPS_153.m
+++ demos/test_SINS_GPS_153.m
@@ -6,11 +6,11 @@
 % 17/06/2011, updated 15/11/2024
 glvs
 psinstypedef(153);
-trj = trjfile('trj10ms.mat');
+trj = trjfile('trj_TL_approach.mat');
 
 % initial settings
 [nn, ts, nts] = nnts(2, trj.ts);
-imuerr = imuerrset(0.05, 200, 0.002, 10);
+imuerr = imuerrset(0.5, 200, 0.2, 10);
 imu = imuadderr(trj.imu, imuerr);
 davp0 = avperrset([0.5;-0.5;20], 0.1, [1;1;3]);
 insAll = insinit(avpadderr(trj.avp0,davp0), ts);
